1ebd44beca779bff73af98b132cc41130b32d289
Nikita <nickitasolonenko@yandex.ru>
2025-04-10 15:55:16 +0300

    solved 8 tasks

---
+++ lab5/src/main/java/ru/npsolonenko/GeneralLock.java
@@ -0,0 +1,31 @@
+package ru.npsolonenko;
+
+import java.util.concurrent.locks.ReentrantLock;
+
+public class GeneralLock {
+    private static final ReentrantLock REENTRANT_LOCK = new ReentrantLock();
+
+    public static AutoCloseable lock() {
+        REENTRANT_LOCK.lock();
+        return createAutoCloseable(REENTRANT_LOCK);
+    }
+
+    public static AutoCloseable lock(ReentrantLock reentrantLock) {
+        reentrantLock.lock();
+        return createAutoCloseable(reentrantLock);
+    }
+
+    private static AutoCloseable createAutoCloseable(ReentrantLock reentrantLock) {
+        return new AutoCloseable() {
+            public void close() {
+                try {
+                    if (reentrantLock.isLocked())
+                        reentrantLock.unlock();
+                }
+                catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+    }
+}
